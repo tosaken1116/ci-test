209a960e308585fc626146294bc6ee144b420cfd
yamato0211 <kumayama0211@gmail.com>
2023-11-10 22:53:23 +0900

    fix: review changes

--- src/components/ui/IconButton/index.stories.tsx
+++ src/components/ui/IconButton/index.stories.tsx
@@ -1,5 +1,3 @@
-import { GitHubLogoIcon } from '@radix-ui/react-icons'
-
 import { IconButton } from '.';
 
 import type { Meta, StoryObj } from '@storybook/react';
@@ -18,62 +16,6 @@ type Story = StoryObj<typeof IconButton>;
 
 export const Default: Story = {
   args: {
-    children: <GitHubLogoIcon />,
-  },
-};
-
-export const Secondary: Story = {
-  args: {
-    variant: 'secondary',
-    children: <GitHubLogoIcon />,
-  },
-};
-
-export const Destructive: Story = {
-  args: {
-    variant: 'destructive',
-    children: <GitHubLogoIcon />,
-  },
-};
-
-export const Outline: Story = {
-  args: {
-    variant: 'outline',
-    children: <GitHubLogoIcon />,
-  },
-};
-
-export const Link: Story = {
-  args: {
-    variant: 'link',
-    children: <GitHubLogoIcon />,
-  },
-};
-
-export const Ghost: Story = {
-  args: {
-    variant: 'ghost',
-    children: <GitHubLogoIcon />,
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: 'sm',
-    children: <GitHubLogoIcon />,
-  },
-};
-
-export const Large: Story = {
-  args: {
-    size: 'lg',
-    children: <GitHubLogoIcon />,
-  },
-};
-
-export const Icon: Story = {
-  args: {
-    size: 'icon',
-    children: <GitHubLogoIcon />,
+    children: <span className='text-xl'>👍</span>
   },
 };
--- src/components/ui/IconButton/index.test.tsx
+++ src/components/ui/IconButton/index.test.tsx
@@ -1,4 +1,3 @@
-import { GitHubLogoIcon } from '@radix-ui/react-icons'
 import { render } from '@testing-library/react';
 
 
@@ -10,52 +9,14 @@ describe('test ui/IconButton', () => {
   it('renders the default variant of the button correctly', () => {
     const {
       container: { firstChild },
-    } = render(<IconButton><GitHubLogoIcon /></IconButton>);
+    } = render(<IconButton><span className='text-xl'>👍</span></IconButton>);
 
-    expect(firstChild).toHaveClass('bg-primary text-primary-foreground');
-  });
-
-  // Test rendering with 'secondary' variant
-  it('renders the secondary variant of the button correctly', () => {
-    const {
-      container: { firstChild },
-    } = render(<IconButton variant="secondary"><GitHubLogoIcon /></IconButton>);
-
-    expect(firstChild).toHaveClass('bg-secondary text-secondary-foreground');
-  });
-
-  // Test rendering with 'destructive' variant
-  it('renders the destructive variant of the button correctly', () => {
-    const {
-      container: { firstChild },
-    } = render(<IconButton variant="destructive"><GitHubLogoIcon /></IconButton>);
-
-    expect(firstChild).toHaveClass(
-      'bg-destructive text-destructive-foreground'
-    );
-  });
-
-  // Test rendering with 'outline' variant
-  it('renders the outline variant of the button correctly', () => {
-    const {
-      container: { firstChild },
-    } = render(<IconButton variant="outline"><GitHubLogoIcon /></IconButton>);
-    
-    expect(firstChild).toHaveClass('border border-input bg-transparent');
-  });
-
-  // Test rendering with 'link' variant
-  it('renders the link variant of the button correctly', () => {
-    const {
-      container: { firstChild },
-    } = render(<IconButton variant="link"><GitHubLogoIcon /></IconButton>);
-
-    expect(firstChild).toHaveClass('text-primary underline-offset-4');
+    expect(firstChild).toHaveClass('text-sm font-medium transition-colors');
   });
 
   // Test that children are rendered properly
   it('renders children correctly', () => {
-    const { container } = render(<IconButton><GitHubLogoIcon /></IconButton>);
-    expect(container.querySelector('svg')).toBeInTheDocument()
+    const { container } = render(<IconButton><span className='text-xl'>👍</span></IconButton>);
+    expect(container.querySelector('span')).toBeInTheDocument()
   });
 });
