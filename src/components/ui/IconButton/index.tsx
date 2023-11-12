ec74216681662ad6103d6c8b09e70a0898a6376f
yamato0211 <kumayama0211@gmail.com>
2023-11-13 00:43:12 +0900

    fix: remove cva

--- src/components/ui/IconButton/index.tsx
+++ src/components/ui/IconButton/index.tsx
@@ -2,19 +2,15 @@ import type { ButtonHTMLAttributes, ReactNode } from "react";
 import { forwardRef } from "react"
 
 import { Slot } from "@radix-ui/react-slot"
-import { cva } from "class-variance-authority"
 
-import type { VariantProps } from "class-variance-authority"
+import { buttonVariants } from "../Button";
 
 import { cn } from "@/libs/utils"
 
-const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
-)
 
-export interface ButtonProps
-  extends ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
+const buttonStyles = "inline-flex items-center justify-center whitespace-nowrap rounded-full text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   asChild?: boolean
 }
 
@@ -35,7 +31,7 @@ const IconButton = forwardRef<HTMLButtonElement, ButtonProps & Props>(
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
-        className={cn(buttonVariants({ className }))}
+        className={cn(buttonStyles, className)}
         ref={ref}
         {...props}
       >
